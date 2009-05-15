da28487874592850b524f9e016ec50a557bc5097
Zachary Zolton <zachary.zolton@gmail.com>
2009-05-14 23:20:39 -0500

    foo-bar fixture

--- spec/cambric_spec.rb
+++ spec/cambric_spec.rb
@@ -1,9 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Cambric" do
+describe "an instance of Cambric" do
 
-  describe "when loading a config file" do
-    
+  describe "when instantiating from a config file" do
     before :all do
       @cambric = Cambric.new(load_fixture 'tweets.yml')
     end
@@ -11,13 +10,25 @@ describe "Cambric" do
     it "should have something for config" do
       @cambric.config.should_not == nil
     end
-    
+  
     it "should contain a key for each database entry" do
       %w(users tweets).each do |db|
         @cambric.config.keys.should include(db)
       end
     end
-    
+  end
+
+  describe "when already instantiated" do
+    before :all do
+      @cambric = Cambric.new(load_fixture 'foo-bar.yml')
+    end
+
+    describe "when creating all databases for an environment" do
+      
+      it "should create a database for each name in cached hash keys config" do
+      end
+      
+    end
   end
 
 end
