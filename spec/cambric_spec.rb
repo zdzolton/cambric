15781e632c2a29fc04aa7b5b3a4535b3c00b9137
Zachary Zolton <zachary.zolton@gmail.com>
2009-05-18 14:12:38 -0500

    more spec refactoring

--- spec/cambric_spec.rb
+++ spec/cambric_spec.rb
@@ -1,78 +1,74 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "an instance of Cambric" do
+describe Cambric do
 
-  describe "when instantiating from a config file" do
-    
-    describe "when not given the design document name" do
-      it "should raise an error" do
-        lambda do 
-          Cambric.new load_fixture('tweets.yml'), :environment => 'development' 
-        end.should raise_error
-      end
-    end
-
-    describe "for a non-specified environment" do
-      it "should raise an error" do
-        lambda do
-          Cambric.new load_fixture('degenerate.yml'), 
-                      :environment => 'staging', 
-                      :design_doc => 'blarg'
-        end.should raise_error
-      end
-    end
-    
-    it "should assume ./couchdb as the database path" do
-      lambda do
-        @cambric = Cambric.new load_fixture('foo-bar-baz.yml'), 
-                               :environment => 'staging', 
-                               :design_doc => 'xop'
-
-        @cambric.push_all_design_docs
+  describe "when initializing without the design document name" do
+    it "should raise an error" do
+      lambda do 
+        Cambric.new load_fixture('tweets.yml'), :environment => 'development' 
       end.should raise_error
     end
+  end
 
-    describe "for an environment specified in the YAML" do
-      before :all do
-        @cambric = Cambric.new load_fixture('twitter-clone.yml'), 
-                               :environment => 'development', 
-                               :design_doc => 'twitter-clone',
-                               :db_dir => File.join(FIXTURES_PATH, 'twitter-clone')
-      end
-    
-      it "should have a value for the environment" do
-        @cambric.environment.should_not be_nil
-      end
-
-      it "should have something for config" do
-        @cambric.config.should_not be_nil
-      end
-      
-      it "should match the given design document name" do
-        @cambric.design_doc_name.should == 'twitter-clone'
-      end
-      
-      it "should match the given database directory" do
-        @cambric.db_dir.should =~ /\/spec\/fixtures\/twitter-clone/
-      end
+  describe "when initializing for a non-specified environment" do
+    it "should raise an error" do
+      lambda do
+        Cambric.new load_fixture('degenerate.yml'), 
+                    :environment => 'staging', 
+                    :design_doc => 'blarg'
+      end.should raise_error
+    end
+  end
   
-      it "should contain a key for each database entry" do
-        %w(users tweets).each do |db|
-          @cambric.config.keys.should include(db)
-        end
-      end
-      
-      it "should hash-style access databases, by name, as string or symbol" do
-        %w(users tweets).each do |db_name|
-          [db_name, db_name.to_sym].each do |db|
-            @cambric[db].uri.should =~ /localhost:5984\/#{db}-development$/
-          end
-        end
+  it "should assume ./couchdb as the database path" do
+    lambda do
+      @cambric = Cambric.new load_fixture('foo-bar-baz.yml'), 
+                             :environment => 'staging', 
+                             :design_doc => 'xop'
+
+      @cambric.push_all_design_docs
+    end.should raise_error
+  end
+
+  describe "after initializing with required information" do
+    before :all do
+      @cambric = Cambric.new load_fixture('twitter-clone.yml'), 
+                             :environment => 'development', 
+                             :design_doc => 'twitter-clone',
+                             :db_dir => File.join(FIXTURES_PATH, 'twitter-clone')
+    end
+  
+    it "should have a value for the environment" do
+      @cambric.environment.should_not be_nil
+    end
+
+    it "should have something for config" do
+      @cambric.config.should_not be_nil
+    end
+    
+    it "should match the given design document name" do
+      @cambric.design_doc_name.should == 'twitter-clone'
+    end
+    
+    it "should match the given database directory" do
+      @cambric.db_dir.should =~ /\/spec\/fixtures\/twitter-clone/
+    end
+
+    it "should contain a key for each database entry" do
+      %w(users tweets).each do |db|
+        @cambric.config.keys.should include(db)
       end
     end
     
+    it "should hash-style access databases, by name, as string or symbol" do
+      %w(users tweets).each do |db_name|
+        [db_name, db_name.to_sym].each do |db|
+          @cambric[db].uri.should =~ /localhost:5984\/#{db}-development$/
+        end
+      end
+    end
   end
-  
+    
   describe "when creating databases" do
     before :all do
       Cambric.new(load_fixture('foo-bar-baz.yml'), 
