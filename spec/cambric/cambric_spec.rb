d373e82385541dd144c4e8794f898194aea48965
Zachary Zolton <zachary.zolton@gmail.com>
2009-06-01 16:16:31 -0500

    moved specs for configurator to a separate script

--- spec/cambric/cambric_spec.rb
+++ spec/cambric/cambric_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 require 'uri'
 
 TWITTER_CLONE_DATABASES = {
@@ -210,35 +210,4 @@ describe Cambric do
     end
   end
 
-end
-
-describe Cambric::Configurator do
-  before :all do
-    @config = Cambric::Configurator.new
-  end
-
-  it "should default its design doc name to 'cambric'" do
-    @config.design_doc_name.should == 'cambric'
-  end
-
-  it "should default to 'cambric' for the database directory" do
-    @config.db_dir.should == 'cambric'
-  end
-
-  it "should default to 'development' for the environment" do
-    @config.environment.should == 'development'
-  end
-  
-  describe "when retrieving the configured CouchRest::Database instances" do
-    before :all do
-      @config.databases = TWITTER_CLONE_DATABASES
-      @dbs = @config.initialize_databases
-    end
-    
-    it "should have the expected URLs for development environment" do
-      %w(users tweets).each do |db|
-        @dbs[db.to_sym].uri.should == "http://127.0.0.1:5984/#{db}-development"
-      end
-    end
-  end
 end
