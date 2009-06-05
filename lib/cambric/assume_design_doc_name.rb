e3bcfff53ade2fdb57625c6cd9cf4efacd92cc1b
Zachary Zolton <zachary.zolton@gmail.com>
2009-06-05 10:41:48 -0500

    added AssumeDesignDocName#get_docs_from_view to cast view result docs to the type given in the :cast_as option

--- lib/cambric/assume_design_doc_name.rb
+++ lib/cambric/assume_design_doc_name.rb
@@ -10,6 +10,16 @@ module Cambric
     def cambric_design_doc
       get "_design/#{@cambric_design_doc_name}"
     end
+    
+    def get_docs_from_view name, options={}
+      cast_as = options.delete(:cast_as) || options.delete('cast_as')
+      results = view name, options.merge(:reduce => false, :include_docs => true)
+      if cast_as
+        results['rows'].map{ |r| cast_as.new(r['doc']) rescue nil }.reject{ |r| r.nil? }
+      else
+        results
+      end
+    end
   
   end
 end
--- spec/cambric/assume_design_doc_name_spec.rb
+++ spec/cambric/assume_design_doc_name_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe Cambric::AssumeDesignDocName do
-  
   describe "after pushing design docs" do
+    
     before :all do
       configure_twitter_clone
       Cambric.prepare_databases!
@@ -17,6 +17,22 @@ describe Cambric::AssumeDesignDocName do
     it "should get the design doc specified by configuration" do
       Cambric[:tweets].cambric_design_doc.should_not be_nil
     end
-  end
-  
+    
+    describe "get_docs_from_view" do
+      before :all do
+        Cambric[:users].save_doc '_id' => 'trevor', 'following' => %w(bob geoff scott brian zach)
+        Cambric[:users].save_doc '_id' => 'scott', 'following' => %w(bob geoff trevor brian zach)
+      end
+      
+      it "should be able specify type to cast query result documents" do
+        user_struct = Struct.new(:hash)
+        followers = Cambric[:users].get_docs_from_view :followers, 
+                                                       :cast_as => user_struct, 
+                                                       :key => 'bob'
+        followers.should have(2).elements
+        followers.each{ |f| f.should be_a(user_struct) }
+      end
+    end
+        
+  end  
 end
